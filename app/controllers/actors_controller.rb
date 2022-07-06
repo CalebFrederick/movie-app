47a8af270181f55d3e99cadd3ce914903fb0da45
CalebFrederick <caleb@heckfamily.com>
2022-07-06 01:47:49 -0400

    added params

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -3,4 +3,9 @@ class ActorsController < ApplicationController
         actor = Actor.find_by(id: params["id"])
         render json: actor.as_json
     end
+
+    def show_all_actors
+        actor = Actor.all
+        render json: actor.as_json
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,15 @@ Rails.application.routes.draw do
 
   get "/actor/:id", controller: "actors", action: "show_actor"
 
+  get "/all_actors", controller: "actors", action: "show_all_actors"
+
   get "/movie/:id", controller: "movies", action: "show_movie"
 
   get "/movies", controller: "movies", action: "show_all_movies"
+
+  get "/actor" => "actors#show_actor"
+
+  get "/movies" => "movies#index"
+
+  get "/movies/:id" => "movies#show_movie"
 end
