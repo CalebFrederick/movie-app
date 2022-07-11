2ad104fe28ba31d80040eda144749846ad424229
CalebFrederick <caleb@heckfamily.com>
2022-07-11 02:55:09 -0400

    added Director column to Movies and Gender / Age to Actors

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -11,18 +11,22 @@ class ActorsController < ApplicationController
 
     def create
         actor = Actor.create(
-            first_name: params["first_name"],
-            last_name: params["last_name"],
-            known_for: params["known_for"],
+            first_name: params[:first_name],
+            last_name: params[:last_name],
+            known_for: params[:known_for],
+            gender: params [:gender],
+            age: params [:age]
         )
     render json: actor.as_json
     end
 
     def update
-        actor = Actor.find_by(id: params["id"])
-        actor.first_name = params["first_name"] || actor.first_name
-        actor.last_name = params["last_name"] || actor.last_name
-        actor.known_for = params["known_for"] || actor.known_for
+        actor = Actor.find_by(id: params[:id])
+        actor.first_name = params[:first_name] || actor.first_name
+        actor.last_name = params[:last_name] || actor.last_name
+        actor.known_for = params[:known_for] || actor.known_for
+        actor.gender = params[:gender] || actor.gender
+        actor.age = params[:age] || actor.age 
         actor.save 
         render json: actor.as_json
     end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,18 +11,22 @@ class MoviesController < ApplicationController
 
     def create
         movie = Movie.create(
-            title: params["title"],
-            year: params["year"],
-            plot: params["plot"],
+            title: params[:title],
+            year: params[:year],
+            plot: params[:plot],
+            director: params[:director],
+            english: params[:english],
         )
         render json: movie.as_json
     end
 
     def update
         movie = Movie.find_by(id: params["id"])
-        movie.title = params["title"] || movie.title
-        movie.year = params["year"] || movie.year
-        movie.plot = params["plot"] || movie.plot
+        movie.title = params[:title] || movie.title
+        movie.year = params[:year] || movie.year
+        movie.plot = params[:plot] || movie.plot
+        movie.director = params[:director] || movie.director
+        movie.english = params[:english] || movie.english
         movie.save
         render json: movie.as_json
     end
