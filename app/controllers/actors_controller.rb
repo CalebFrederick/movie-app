794792bf90dc05046f0fe5374a1d0fc3d4f5c6c0
CalebFrederick <caleb@heckfamily.com>
2022-07-13 03:15:12 -0400

    Added Movie_ID and has_many associations

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -15,7 +15,8 @@ class ActorsController < ApplicationController
             last_name: params[:last_name],
             known_for: params[:known_for],
             gender: params [:gender],
-            age: params [:age]
+            age: params [:age],
+            movie_id: params [:movie_id]
         )
 
         if actor.create
@@ -31,7 +32,8 @@ class ActorsController < ApplicationController
         actor.last_name = params[:last_name] || actor.last_name
         actor.known_for = params[:known_for] || actor.known_for
         actor.gender = params[:gender] || actor.gender
-        actor.age = params[:age] || actor.age 
+        actor.age = params[:age] || actor.age
+        actor.movie_id = params[:movie_id] || actor.movie_id 
         actor.save 
         render json: actor.as_json
 
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
+  has_many :actors
+  
   validates :title, presence: true
   validates :director, presence: true
   validates :year, numericality: true
